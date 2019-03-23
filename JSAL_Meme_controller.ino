1f87d9d5eef26dbfaf29a9c2c39b651ad7ad7eba
calluminfra <43877033+calluminfra@users.noreply.github.com>
2019-03-23 17:57:26 +0000

    Changes

--- JSAL_Meme_controller.ino
+++ JSAL_Meme_controller.ino
@@ -1,24 +1,26 @@
 /*
- * Pinout!!!
  * 
+ * TO DO:
+ * ---
+ * Fix RMB
+ * Tweak Gyro to reduce drifte
+ * ---
+ * 
+ * Pinout!!!
  * -------
  * Analog
  * -------
- * DONE >>> Moisture Sensor -> A1 -> LMB Hold (Val ~ <200)     <<< DONE
- * 
- * 
- * 
+ * DONE >>> Moisture Sensor -> A1 -> LMB Hold (Val ~ <200)      <<< DONE
+ * DONE >>> Potentiometer -> A0 -> Shift/Ctrl                   <<< DONE
+ * DONE >>> Metal Touch Sensor (Prev Shock  -> A2 -> RMB click  <<< DONE
  * -------
  * Digital
  * -------
- * DONE >>> GyroScope -> 12 (SCL) 11 (SDA) -> Mouse Movement  <<< DONE
- * DONE >>> Buttons - > D7 D6 D4 D5 -> W A S D                <<< DONE
- * DONE >>> Shock Sensor -> 2 -> RMB click                    <<< DONE
- * DONE >>> Microphone -> 1 -> Space                          <<< DONE
- * Rotary Encoder Turn -> 3 0 -> Mouse Wheel up/down (Use interrupts?)
- * Rotary Encoder Click -> 8 -> E (Use interrupts)
- * DONE >>> Potentiometer -> A0 -> Shift/Ctrl (Use Interrupts) <<< DONE
- * 
+ * DONE >>> GyroScope -> 12 (SCL) 11 (SDA) -> Mouse Movement    <<< DONE
+ * DONE >>> Buttons - > D7 D6 D4 D5 -> W A S D                  <<< DONE
+ * DONE >>> Microphone -> 1 -> Space                            <<< DONE
+ * DONE >>> Rotary Encoder Turn -> 3 0 -> Mouse Wheel up/down   <<< DONE
+ * DONE >>> Rotary Encoder Click -> 8 -> E                      <<< DONE
  * 
  */
 
@@ -48,7 +50,7 @@ MPU6050 mpu;
 uint8_t micPin = 1;
 uint8_t potPin = A0;
 uint8_t moisturePin = A1;
-uint8_t shockPin = 2;
+uint8_t shockPin = A2;
 uint8_t encoderPinA = 3;
 uint8_t encoderPinB = 8;
 
@@ -57,6 +59,9 @@ char buttonCharacters[5] = {'w', 'a', 's', 'd', 'e'};
 unsigned long timeStamp = 0;
 
 uint8_t numberOfButtons = 5;
+
+volatile bool shockState = 0;
+volatile bool shockFlag = 0;
 
 void setup(){
   Serial.begin(115200);
@@ -76,10 +81,11 @@ void setup(){
   //Pin setups
   pinMode(micPin, INPUT);
   pinMode(potPin, INPUT);
+  pinMode(shockPin, INPUT);
   pinMode(moisturePin, INPUT);
   pinMode(encoderPinA, INPUT);
   pinMode(encoderPinB, INPUT);
-  
+
 } 
 
 void loop(){
@@ -133,9 +139,9 @@ void sampleButtons(){
 void sampleGyro(){
   int16_t ax, ay, az, gx, gy, gz;
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  int vx = (gx + 300) / 200;
-  int vy = -(gz - 100) / 200;
-
+  int vx = (gx + 200) / 200;
+  int vy = -(gz + 50) / 200;
+  
   Mouse.move(vx, vy);
 }
 
@@ -143,37 +149,52 @@ void sampleGyro(){
 void sampleMic(){
   static uint8_t prevMicState;  
   static unsigned long lastMicTs = 0;
-  static uint16_t micDelay = 120;
+  static uint16_t micDelay = 300;
   
   bool micState = digitalRead(micPin);
   prevMicState = (prevMicState << 1);
   prevMicState |= micState;
-  if ((millis() > (lastMicTs + micDelay)) && prevMicState == 0b01111111){
+  if ((millis() > (lastMicTs + micDelay)) && micState == 1){
     Serial.println("Microphone debounced to HIGH state");
     lastMicTs = millis();
     //Write Space
-   // Keyboard.write(0x20);
+    Keyboard.press(0x20);
+  }
+  else if (prevMicState == 0b10000000){
+    Keyboard.release(0x20);
   }
 }
 
 /*RMB*/
 /*!!! ADD MOUSE PRESS !!!*/
 void sampleShock(){
-  static uint8_t shockBuffer = 0;
-  static uint8_t shockState = 0;
-  uint8_t shockSample = digitalRead(shockPin);
-  shockBuffer = shockBuffer << 1;
-  shockBuffer |= shockSample;
+  uint16_t shockSample = analogRead(shockPin);
+  //Serial.println(shockSample);
+  static unsigned long shockDebounceTs = millis();
+  const uint16_t shockDebounceDelay = 120;
+  static bool shockState = 0;
 
-  if (shockState == 0 && shockBuffer == 0b01111111){
-    shockState = 1;
-    Serial.println("RMB Released");
+  switch (shockState){
+    case 0:
+      shockDebounceTs = millis();
+      //Button has been pressed
+      if (shockSample < 800){
+        shockState = 1;
+        Serial.println("RMB Pressed");
+        Mouse.press(MOUSE_RIGHT);
+      }
+    break;
+    case 1:
+      //Falling debounce has occurred
+      if (millis() > (shockDebounceTs + shockDebounceDelay)){
+        if (shockSample >= 1023){
+          shockState = 0;
+          Serial.println("RMB Released");
+          Mouse.release(MOUSE_RIGHT);
+        }
+      }
+    break;
   }
-  else if (shockState == 1 && shockBuffer == 0b10000000){
-    shockState = 0;
-    Serial.println("RMB Pressed");
-  }
-  
 }
 
 /*LMB*/
@@ -188,10 +209,12 @@ void sampleMoisture(){
   if (moistureState == 0 && moistureSample > highSample){
     moistureState = 1;
     Serial.println("LMB Pressed");
+    Mouse.press(MOUSE_LEFT);
   }
   else if(moistureState == 1 && moistureSample < lowSample){
     moistureState = 0;
     Serial.println("LMB Released");
+    Mouse.release(MOUSE_LEFT);
   }
 }
 
@@ -205,9 +228,11 @@ void sampleRotary(){
   if((lastPinASample == 1) && (pinASample == 0)){
     if (digitalRead(encoderPinB) == 0){
       Serial.println("Mouse wheel scrolldown");
+      Mouse.move(0,0,-1);
     }
     else{
       Serial.println("Mouse wheel scrollup");
+      Mouse.move(0,0,1);
     }
   }
   lastPinASample = pinASample;
@@ -232,4 +257,3 @@ void samplePot(){
     Keyboard.release(0x2A);
   }
 }
-
